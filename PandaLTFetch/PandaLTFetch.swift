3745b3fbfb6785618d86aba0a2055718bae6479a
tinaxd <31384595+tinaxd@users.noreply.github.com>
2020-10-05 19:34:01 +0900

    first

--- PandaLTFetch/PandaLTFetch.swift
+++ PandaLTFetch/PandaLTFetch.swift
@@ -2,9 +2,28 @@
 //  PandaLTFetch.swift
 //  PandaLTFetch
 //
-//  Created by Chinamu Kawano on 2020/10/05.
+//  Created by tinaxd on 2020/10/05.
 //
 
-class PandaLTFetch {
+import Alamofire
+import Foundation
 
+public struct PandaLT {
+    public static func getLoginTokenAsync(success: @escaping (String) -> Void, fail: @escaping () -> Void) {
+        AF.request("https://cas.ecs.kyoto-u.ac.jp/cas/login").responseString {
+            response in
+            let regex = try! NSRegularExpression(pattern: "<input type=\"hidden\" name=\"lt\" value=\"(.+)\" \\/>");
+            switch response.result {
+            case .failure(_): fail();
+            case let .success(str):
+                guard let result = regex.firstMatch(in: str, options: [], range: NSRange(0..<str.count)) else {
+                    fail();
+                    return;
+                }
+                let start = result.range(at: 1).location;
+                let end = start + result.range(at: 1).length;
+                success(String(str[str.index(str.startIndex, offsetBy: start)..<str.index(str.startIndex, offsetBy: end)]));
+            }
+        }
+    }
 }
